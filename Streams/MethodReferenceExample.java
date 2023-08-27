14137b06b81146f61e3c964c8f5e03befb3a971f
Shariq Shaikh <shariq.shaikh@paysyslabs.com>
2023-08-27 14:51:11 +0500

    Updated Method Reference Code

--- Streams/MethodReferenceExample.java
+++ Streams/MethodReferenceExample.java
@@ -2,15 +2,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+
+interface B{
+    A getMessage(String message);
+}
+
+
 class A{
 
 
     A(String name){
         this.name=name;
+        System.out.println(name);
     }
 
      String name;
-    public static void print(String name){
+    public static void print(A name){
         System.out.println("Static Method"+name);
 
     }
@@ -23,9 +30,9 @@ class A{
         return name;
     }
 
-    public List<String> getList(){
+    public List<A> getList(){
        return IntStream.range(1, 10)
-        .mapToObj(i-> " Employee is "+new A("Shariq"))
+        .mapToObj(i-> new A("Shariq"))
         .collect(Collectors.toList());
     }
 
@@ -36,9 +43,25 @@ class A{
 public class MethodReferenceExample{
 
     public static void main(String[] args) {
-       A a=new A("Shariq");
+      //Static Method Reference
+        A a=new A("Shariq");
        a.getList()
        .forEach(A::print);
+
+
+       //instance method Reference
+         A a1=new A("Shariq");
+       a1.getList()
+       .forEach(A::print2);
+
+
+        //Constructor Reference
+         B a2=A::new;
+         a2.getMessage("Something");
+         
+
+      
+        
         
     }
 }
